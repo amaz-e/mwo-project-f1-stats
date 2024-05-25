2cf8c5dac01aa56bc8a82556ceaaba807bc0dd7f
amaz-e <sonicmephit@gmail.com>
2024-05-25 12:11:56 +0200

    Initial project setup

--- src/main/java/pl/edu/agh/mwo/App.java
+++ src/main/java/pl/edu/agh/mwo/App.java
@@ -1,13 +1,18 @@
 package pl.edu.agh.mwo;
 
+import java.net.MalformedURLException;
+
+import static pl.edu.agh.mwo.PitstopParser.JSONtoObjectList;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws MalformedURLException {
         System.out.println( "Hello World!" );
+        JSONtoObjectList();
     }
 }
